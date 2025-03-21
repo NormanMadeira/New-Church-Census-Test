ea463c84991f6145cdb1c89c85cd58e4fc40a796
NormanMadeira <112630200+NormanMadeira@users.noreply.github.com>
2025-03-21 23:45:01 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyD_QusNDRMcRO5VtlRV5UMwrVnrRh-vPgQ",
+  authDomain: "new-church-census-test.firebaseapp.com",
+  projectId: "new-church-census-test",
+  storageBucket: "new-church-census-test.appspot.com",
+  messagingSenderId: "380854562551",
+  appId: "1:380854562551:web:98a768d575e38032372df2"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
